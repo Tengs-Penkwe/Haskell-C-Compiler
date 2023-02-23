058c9850dda24ef5df054a77d19c5176a7bcf723
Tengs Penkwe <tengspenkwe@gmail.com>
2023-02-23 14:49:30 -0800

    read from file

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,20 @@ module Main where
 import Parser
 
 import Control.Monad.Trans
+
+import System.IO                --readFile
+import System.Environment       -- getArgs
 import System.Console.Haskeline
 
 process :: String -> IO ()
-process line = do
-  let res = getStatements line
+process content = do
+  let res = parseProgram content
   case res of 
     Left err -> print err
     Right ex -> mapM_ print ex
 
-main :: IO()
-main = runInputT defaultSettings loop
+repl :: IO()
+repl = runInputT defaultSettings loop
   where
   loop = do
     minput <- getInputLine "ready> "
@@ -21,3 +24,9 @@ main = runInputT defaultSettings loop
       Nothing -> outputStrLn "Goodbye."
       Just input -> (liftIO $ process input) >> loop
 
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    []      -> repl
+    [file]  -> readFile file >>= process
