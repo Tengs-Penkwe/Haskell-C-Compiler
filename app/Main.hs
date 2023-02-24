7f34c63f6e5dbb2f7a648a22b9cc0170a042cc40
Tengs Penkwe <tengspenkwe@gmail.com>
2023-02-23 19:25:56 -0800

    write result to file

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,24 @@
 module Main where 
 
 import Parser
+import Codegen
 
 import Control.Monad.Trans
 
-import System.IO                --readFile
-import System.Environment       -- getArgs
+import System.IO (readFile, writeFile)
+import System.Environment (getArgs)
 import System.Console.Haskeline
+import System.Console.GetOpt
 
-process :: String -> IO ()
+process :: String -> IO String
 process content = do
   let res = parseProgram content
   case res of 
-    Left err -> print err
-    Right ex -> mapM_ print ex
+    Left err -> print err >> return ""
+    Right ex -> do
+      let ir = genProgram ex  
+      mapM_ print ex
+      return ir
 
 repl :: IO()
 repl = runInputT defaultSettings loop
@@ -29,4 +34,7 @@ main = do
   args <- getArgs
   case args of
     []      -> repl
-    [file]  -> readFile file >>= process
+    [file]  -> do  --readFile file >>= process
+      content <- readFile file
+      ir      <- process content
+      writeFile "output.ll" ir
